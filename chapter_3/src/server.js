28b5115d095f4e397c69deeac49dd3be50fcbc39
Sumit Kumar Shaw <shawsumit6286@gmail.com>
2025-01-25 15:53:49 +0530

    Add files via upload

---
+++ chapter_3/src/server.js
@@ -0,0 +1,37 @@
+import express from "express"
+import path, { dirname } from 'path'
+import { fileURLToPath } from 'url'
+import authRoutes from './routes/authRoutes.js'
+import todoRoutes from './routes/todoRoutes.js' 
+import authMiddleware from "./middleware/authMiddleware.js"
+
+const app = express()
+const PORT = process.env.PORT || 5003
+
+// get the file path from the URL of the current module
+const __filename = fileURLToPath(import.meta.url)
+// get the directory name from the file path 
+const __dirname = dirname(__filename)
+
+//MIDDLEWARE
+app.use(express.json())
+
+
+// Serves the html file from the /publiv directory.
+// Tells express to serve all files from the public folder as static assets /file.
+// Any requests for the css files will be resolved to the public directory.
+
+app.use(express.static(path.join(__dirname, '../public')))
+
+// Serving up the html file from the /public directory
+app.get('/', (req,res)=>{
+    res.sendFile(path.join(__dirname, 'public', 'index.html'))
+})
+
+// Routes
+app.use('/auth', authRoutes) 
+app.use('/todos',authMiddleware, todoRoutes) 
+
+app.listen(PORT, () => {
+    console.log(`server started at port: ${PORT}`)
+})
